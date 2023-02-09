84e07d75e598560baeb18314e9730facfda19557
Alice <alicewengs@gmail.com>
2023-02-09 11:17:05 -0800

    created a route for /moods

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
     console.log("Connected to database.");
 });
 
+app.use("/moods", require("./controllers/moods"));
+
 app.listen(PORT, () => {
     console.log("Server is running.");
 });
