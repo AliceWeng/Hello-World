96ef35b8d94f0460530f73cd67bf4827a2e1288a
Alice <alicewengs@gmail.com>
2023-02-09 08:50:03 -0800

    required dependencies in server.js

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const PORT = process.env.PORT;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log(`Connected to database: ${process.env.MONGO_URI}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
