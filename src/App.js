68d613991b675a78ac652713ed9be9f247557d14
Alice <alicewengs@gmail.com>
2023-02-09 13:32:18 -0800

    installed react-router-dom to frontend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import SignUpForm from "./Components/SignUpForm";
 import MoodForm from "./Components/MoodForm";
@@ -5,8 +6,22 @@ import MoodForm from "./Components/MoodForm";
 function App() {
   return (
     <div>
-      <SignUpForm/>
-      <MoodForm/>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/moods/new" element={<MoodForm/>}/>
+          <Route path="/signup" element={<SignUpForm/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
