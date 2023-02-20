481ee7401af231ac996e271ae6ceca19d597c35e
Alice <alicewengs@gmail.com>
2023-02-20 09:20:58 -0800

    added a handle change function to save inputs to state

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -34,20 +34,23 @@ function SignUpForm() {
         }
     }
 
+    const handleChange = e => {
+        setUser({...user, [e.target.id]: e.target.value});
+    }
     return (
         <div className="center">
             <form onSubmit={handleSubmit}>
                 <h1>Sign Up</h1>
                 <label htmlFor="nickname">Nickname</label>
-                <input id="nickname" name="nickname" type="text" required/>
+                <input id="nickname" name="nickname" type="text" onChange={handleChange} required/>
                 <label htmlFor="username">Username</label>
-                <input id="username" name="username" type="text" required/>
+                <input id="username" name="username" type="text" onChange={handleChange} required/>
                 <label htmlFor="password">Password</label>
-                <input id="password" name="password" type="text" required/>
+                <input id="password" name="password" type="text" onChange={handleChange} required/>
                 <label htmlFor="email">Email</label>
-                <input id="email" name="email" type="email" required/>
+                <input id="email" name="email" type="email" onChange={handleChange} required/>
                 <label htmlFor="birthday">Birthday</label>
-                <input id="birthday" name="birthday" type="date" required/>
+                <input id="birthday" name="birthday" type="date" onChange={handleChange} required/>
                 <input className="submit" type="submit"/>
             </form>
         </div>
