395e061ae55faa339345b1af7b7a588de306961a
Alice <alicewengs@gmail.com>
2023-03-28 13:00:52 -0700

    changed main to index.js in package.json

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log("Connected to database.");
 });
@@ -29,7 +29,7 @@ app.use("/api/user", require("./controllers/user_controller"));
 app.use("/api/moods", require("./controllers/moods_controller"));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 app.listen(PORT, () => {
